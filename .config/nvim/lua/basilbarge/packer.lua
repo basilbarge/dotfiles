ff8a529ea919aeb42f35bf6021f3d0befcd690ee
Zach Barge <zachb.barge@gmail.com>
2023-05-30 20:16:27 -0400

    Changed theme to dracula.

--- .config/nvim/lua/basilbarge/packer.lua
+++ .config/nvim/lua/basilbarge/packer.lua
@@ -12,9 +12,14 @@ return require('packer').startup(function(use)
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
+--  use({
+--	  'rose-pine/neovim',
+--	  as = 'rose-pine',
+--  })
+
   use({
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
+	  'Mofiqul/dracula.nvim',
+	  as = 'dracula',
   })
   use( 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'})
   use 'nvim-treesitter/nvim-treesitter-context'

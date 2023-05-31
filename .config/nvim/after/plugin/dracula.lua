39addfbbc18683844e066d0ec40850726093d505
Zach Barge <zachb.barge@gmail.com>
2023-05-30 20:15:34 -0400

    Changed theme to Dracula

---
+++ .config/nvim/after/plugin/dracula.lua
@@ -0,0 +1,13 @@
+-- require('rose-pine').setup({
+-- 	--- @usage 'auto'|'main'|'moon'|'dawn'
+-- 	variant = 'moon',
+-- 	--- @usage 'main'|'moon'|'dawn'
+-- 	disable_italics = true,
+-- })
+-- 
+-- 
+-- vim.cmd.colorscheme "rose-pine"
+require('dracula').setup({
+	show_end_of_buffer = true,
+})
+vim.cmd.colorscheme "dracula"
--- .config/nvim/after/plugin/rosepine.lua
+++
@@ -1,9 +0,0 @@
-require('rose-pine').setup({
-	--- @usage 'auto'|'main'|'moon'|'dawn'
-	variant = 'moon',
-	--- @usage 'main'|'moon'|'dawn'
-	disable_italics = true,
-})
-
-
-vim.cmd.colorscheme "rose-pine"

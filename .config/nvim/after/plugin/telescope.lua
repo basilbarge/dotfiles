fe17ef98839dc9a9f68a491720d1ffc39fd9a64b
Zach Barge <zachb.barge@gmail.com>
2023-06-01 19:02:28 -0400

    Reconfigured telescope keybindings.

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,5 +1,18 @@
 local builtin = require('telescope.builtin')
 
+local actions = require("telescope.actions")
+require("telescope").setup{
+  defaults = {
+    mappings = {
+      i = {
+        ["<esc>"] = actions.close,
+        ["<C-k>"] = actions.move_selection_previous,
+        ["<C-j>"] = actions.move_selection_next,
+      },
+    },
+  }
+}
+
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 
 vim.keymap.set('n', '<leader>gf', builtin.git_files, {})

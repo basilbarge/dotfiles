81056fdc9a43fe14bf6ffad04b8c23ceeb740401
Zach Barge <zachb.barge@gmail.com>
2023-06-18 10:43:50 -0400

    Re-added netrw keybinding.

--- .config/nvim/lua/basilbarge/remap.lua
+++ .config/nvim/lua/basilbarge/remap.lua
@@ -1,6 +1,7 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("i", "<leader>jk", "<ESC>")
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y"]])
 vim.keymap.set("n", "<leader Y", [["+Y"]])
 

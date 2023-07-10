20695b259c321ba1f63a6abc73c22d096d056666
Zach Barge <zachb.barge@gmail.com>
2023-07-10 12:06:23 -0400

    Re-added nvim config to .config

---
+++ .config/nvim/after/plugin/lsp.lua
@@ -0,0 +1,22 @@
+local lsp = require('lsp-zero').preset({"recommended"})
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+	['C-p'] = cmp.mapping.select_prev_item(cmp_select),
+	['C-n'] = cmp.mapping.select_next_item(cmp_select),
+	['C-y'] = cmp.mapping.confirm({select = true}),
+})
+
+lsp.setup_nvim_cmp({
+	mapping = cmp_mappings
+})
+
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({})
+end)
+
+-- (Optional) Configure lua language server for neovim
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+lsp.setup()
---
+++ .config/nvim/lua/basilbarge/remap.lua
@@ -0,0 +1,37 @@
+vim.g.mapleader = " "
+
+vim.keymap.set("i", "<leader>jk", "<ESC>")
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y"]])
+vim.keymap.set("n", "<leader Y", [["+Y"]])
+
+vim.keymap.set("", "<up>", "<noop>")
+vim.keymap.set("", "<down>", "<noop>")
+vim.keymap.set("", "<left>", "<noop>")
+vim.keymap.set("", "<right>", "<noop>")
+
+local set = vim.opt
+
+vim.cmd("set autochdir")
+--set.autochdir = true
+set.autoindent = true
+set.tabstop = 4
+set.shiftwidth = 4
+set.softtabstop = 4
+set.number = true
+set.relativenumber = true
+
+set.termguicolors = true
+
+vim.o.clipboard = 'unnamedplus'
+
+if vim.fn.has('wsl') == 1 then
+    vim.api.nvim_create_autocmd('TextYankPost', {
+        group = vim.api.nvim_create_augroup('Yank', { clear = true }),
+        callback = function()
+            vim.fn.system('clip.exe', vim.fn.getreg('"'))
+        end,
+    })
+end
+
+vim.g.netrw_keepdir = 0

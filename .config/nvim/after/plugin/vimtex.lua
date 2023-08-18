9033480fb1d7105544b5ff54f286fb13e826cb87
Zach Barge <zachb.barge@gmail.com>
2023-08-18 14:13:08 -0400

    Updated vimtex confiugration.

--- .config/nvim/after/plugin/vimtex.lua
+++ .config/nvim/after/plugin/vimtex.lua
@@ -2,3 +2,5 @@ vim.api.nvim_command("filetype plugin indent on");
 vim.api.nvim_command("syntax enable");
 
 vim.api.nvim_command("let g:vimtex_view_method = 'zathura'");
+
+vim.api.nvim_command("let maplocalleader = \".\"");

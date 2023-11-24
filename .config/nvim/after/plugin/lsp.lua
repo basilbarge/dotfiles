f04c0a434e95b4b4cebfdd5b1b9ac890ff2a31d4
Zach Barge <zachb.barge@gmail.com>
2023-11-24 13:43:32 -0500

    Updated lsp plugin code to match creators default usage example.

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -16,6 +16,13 @@ lsp.on_attach(function(client, bufnr)
   lsp.default_keymaps({})
 end)
 
+require('mason').setup({});
+require('mason-lspconfig').setup({
+	ensure_installed = {},
+	handlers = {
+		lsp.default_setup,
+	}
+});
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 

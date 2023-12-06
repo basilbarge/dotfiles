bfb11a1bb6ae28309a2844834b65e240e9d620df
Zach Barge <zachb.barge@gmail.com>
2023-12-06 12:26:49 -0500

    Added lspkind configuration to lsp.lua

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -26,4 +26,22 @@ require('mason-lspconfig').setup({
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
+local lspkind = require('lspkind')
+
 lsp.setup()
+
+cmp.setup {
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = 'symbol', -- show only symbol annotations
+      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+
+	  -- The function below will be called before any actual modifications from lspkind
+	  -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+	  before = function (entry, vim_item)
+        return vim_item
+      end
+    })
+  }
+}

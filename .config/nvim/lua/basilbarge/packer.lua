ab019959c779f855f24c8ab96b0646154957869d
Zach Barge <zachb.barge@gmail.com>
2023-12-19 12:26:19 -0500

    Updated harpooon and nvim packages.

--- .config/nvim/lua/basilbarge/packer.lua
+++ .config/nvim/lua/basilbarge/packer.lua
@@ -4,54 +4,54 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  use 'norcalli/nvim-colorizer.lua'
-  use { 'catppuccin/nvim', as = 'catppuccin' }
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		-- or                            , branch = '0.1.x',
+		requires = { { 'nvim-lua/plenary.nvim' } }
+	}
+	use 'norcalli/nvim-colorizer.lua'
+	use { 'catppuccin/nvim', as = 'catppuccin' }
 
-  use( 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'})
-  use 'nvim-treesitter/nvim-treesitter-context'
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v2.x',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},             -- Required
-		  {                                      -- Optional
-		  'williamboman/mason.nvim',
-		  run = function()
-			  pcall(vim.cmd, 'MasonUpdate')
-		  end,
-	  },
-	  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+	use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+	use 'nvim-treesitter/nvim-treesitter-context'
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v2.x',
+		requires = {
+			-- LSP Support
+			{ 'neovim/nvim-lspconfig' }, -- Required
+			{                  -- Optional
+				'williamboman/mason.nvim',
+				run = function()
+					pcall(vim.cmd, 'MasonUpdate')
+				end,
+			},
+			{ 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-	  -- Autocompletion
-	  {'hrsh7th/nvim-cmp'},     -- Required
-	  {'hrsh7th/cmp-nvim-lsp'}, -- Required
-	  {'L3MON4D3/LuaSnip'},     -- Required
-  }
-}
-use 'lervag/vimtex'
-use 'nvim-lua/plenary.nvim'
-use {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    requires = { {"nvim-lua/plenary.nvim"} }
-}
-use ({
-	'nvimdev/lspsaga.nvim',
-	after = 'nvim-lspconfig',
-	config = function()
-		require('lspsaga').setup({})
-	end,
-})
-use 'lewis6991/gitsigns.nvim'
-use('neovim/nvim-lspconfig')
-use('onsails/lspkind.nvim')
+			-- Autocompletion
+			{ 'hrsh7th/nvim-cmp' }, -- Required
+			{ 'hrsh7th/cmp-nvim-lsp' }, -- Required
+			{ 'L3MON4D3/LuaSnip' }, -- Required
+		}
+	}
+	use 'lervag/vimtex'
+	use 'nvim-lua/plenary.nvim'
+	use {
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		requires = { { "nvim-lua/plenary.nvim" } }
+	}
+	use({
+		'nvimdev/lspsaga.nvim',
+		after = 'nvim-lspconfig',
+		config = function()
+			require('lspsaga').setup({})
+		end,
+	})
+	use 'lewis6991/gitsigns.nvim'
+	use('neovim/nvim-lspconfig')
+	use('onsails/lspkind.nvim')
 end)

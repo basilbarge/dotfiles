f809846d845ca70587691306db7c5594dea7a7c9
Zach Barge <zachb.barge@gmail.com>
2024-01-16 14:16:16 -0500

    Refactored file system to accomodate lazy package manager.

--- .config/nvim/after/plugin/catppuccin.lua
+++
@@ -1,4 +0,0 @@
-vim.cmd.colorscheme "catppuccin-mocha"
-
-vim.cmd [[hi IncSearch guifg=#181825 guibg=#ffe4e1]]
-vim.cmd [[syntax on]]
--- .config/nvim/after/plugin/vimtex.lua
+++
@@ -1,6 +0,0 @@
-vim.api.nvim_command("filetype plugin indent on");
-vim.api.nvim_command("syntax enable");
-
-vim.api.nvim_command("let g:vimtex_view_method = 'zathura'");
-
-vim.api.nvim_command("let maplocalleader = \".\"");

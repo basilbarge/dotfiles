0a18d7772b37d95b86621d4acec774df96600938
Zach Barge <zachb.barge@gmail.com>
2024-01-18 17:02:29 -0500

    Formatted file.

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,8 @@
 return {
-	'nvim-telescope/telescope.nvim', branch = '0.1.x',
-	dependencies =  { 'nvim-lua/plenary.nvim' },
-	config = function() 
+	'nvim-telescope/telescope.nvim',
+	branch = '0.1.x',
+	dependencies = { 'nvim-lua/plenary.nvim' },
+	config = function()
 		local builtin = require('telescope.builtin')
 
 		local actions = require("telescope.actions")
@@ -16,9 +17,7 @@ return {
 		}
 
 		vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-
 		vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
-
 		vim.keymap.set('n', '<leader>ps', function()
 			builtin.grep_string({ search = vim.fn.input("Grep > ") })
 		end)

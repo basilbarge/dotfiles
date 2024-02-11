ba3a4c77a89ab1657c36f50800131140b33719c2
Zach Barge <zachb.barge@gmail.com>
2024-02-11 06:43:27 -0500

    Added some code to get nvm to be able to run in current terminal session.

--- install.sh
+++ install.sh
@@ -90,7 +90,11 @@ sudo ln -s $PWD/.config/* ~/.config
 
 # Install nvm and LTS of node
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-nvm install --lts
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install --lts
 # Source bashrc to get latest configuration
 . ~/.bashrc

519d716cbf79d2270f254405d33491701e3f3314
basilbarge <basilbarge@Ubuntu.myguest.virtualbox.org>
2024-03-18 16:18:19 -0400

    Fixed install.sh to make symlinks to the correct  dir

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@
 . /etc/os-release
 echo $NAME
 
+DOTFILES_REPO=$PWD
+
 if [[ $NAME = "Ubuntu" ]]; then
 	echo "Installing packages in Ubuntu"
 	yes | sudo apt install neofetch
@@ -53,40 +55,46 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 
 # Create Symbolic links to dotfiles
-if [ -f $HOME/.bash_aliases ]; then
+if [ -f $HOME/.bash_aliases ] || [ -L $HOME/.bash_aliases ]; then
+	echo "Removing .bash_aliases from home directory"
 	sudo rm $HOME/.bash_aliases
 fi
-sudo ln -s $PWD/.bash_aliases ~
+sudo ln -s $DOTFILES_REPO/.bash_aliases ~
 
-if [ -f $HOME/.bashrc ]; then
+if [ -f $HOME/.bashrc ] || [ -L $HOME/.bashrc ]; then
+	echo "Removing .bash_rc from home directory"
 	sudo rm $HOME/.bashrc
 fi
-sudo ln -s $PWD/.bashrc ~
+sudo ln -s $DOTFILES_REPO/.bashrc ~
 
-if [ -f $HOME/.inputrc ]; then
+if [ -f $HOME/.inputrc ] || [ -L $HOME/.inputrc ]; then
+	echo "Removing .inputrc from home directory"
 	sudo rm $HOME/.inputrc
 fi
-sudo ln -s $PWD/.inputrc ~
+sudo ln -s $DOTFILES_REPO/.inputrc ~
 
-if [ -f $HOME/.tmux.conf ]; then
+if [ -f $HOME/.tmux.conf ] || [ -L $HOME/.tmux.conf ]; then
+	echo "Removing .tmux.conf from home directory"
 	sudo rm $HOME/.tmux.conf
 fi
-sudo ln -s $PWD/.tmux.conf ~
+sudo ln -s $DOTFILES_REPO/.tmux.conf ~
 
-for ITEM in $PWD/.config/*; 
+for ITEM in $DOTFILES_REPO/.config/*; 
 do
 	BASE=$(basename $ITEM)
-	if [ -d $PWD/.config/$BASE ]; then
-		if [ -d $HOME/.config/$BASE ]; then
+	if [ -d $DOTFILES_REPO/.config/$BASE ]; then
+		if [ -d $HOME/.config/$BASE ] || [ -L $HOME/.config/$BASE ]; then
+			echo "Removing directories from .config in home directory"
 			sudo rm -r $HOME/.config/$BASE
 		fi
-	elif [ -f $PWD/.config/$ITEM ]; then
-		if [ -f $HOME/.config/$BASE ]; then
+	elif [ -f $DOTFILES_REPO/.config/$ITEM ]; then
+		if [ -f $HOME/.config/$BASE ] || [ -L $HOME/.config/$BASE ]; then
+			echo "Removing files from .config in home directory"
 			sudo rm $HOME/.config/$BASE
 		fi
 	fi
 done
-sudo ln -s $PWD/.config/* ~/.config
+sudo ln -s $DOTFILES_REPO/.config/* ~/.config
 
 # Install nvm and LTS of node
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash

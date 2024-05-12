40bbba8fb06844dccebb17446eb5b770f08053cb
Zachary Barge <zachb.barge@gmail.com>
2024-05-12 18:07:32 -0400

    changed remap again.

--- .config/nvim/lua/plugins/vim-fugitive.lua
+++ .config/nvim/lua/plugins/vim-fugitive.lua
@@ -2,6 +2,6 @@ return {
 	'tpope/vim-fugitive',
 	config = function()
 		vim.keymap.set('n', '<leader>g', '<cmd>G<CR>')
-		vim.keymap.set('n', '<leader>gP', '<cmd>G push<CR>')
+		vim.keymap.set('n', '<leader>gp', '<cmd>G push<CR>')
 	end
 }

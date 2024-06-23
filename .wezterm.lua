f5962b500b149bd4d9847671839dd2ab5c6733a1
Zachary Barge <zachb.barge@gmail.com>
2024-06-23 16:59:27 -0400

    Changed wezterm font and colorscheme in config

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,8 +2,8 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- Configure fonts
-config.font = wezterm.font 'SpaceMono Nerd Font Mono'
-config.color_scheme = "tokyonight"
+config.font = wezterm.font 'MonaspiceKr Nerd Font Mono'
+config.color_scheme = "Catppuccin Mocha"
 
 return config
 

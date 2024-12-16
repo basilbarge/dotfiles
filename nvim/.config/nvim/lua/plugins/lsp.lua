8465bcc0f8a8357067e99264da75b88d4deb55f3
basilbarge <zachb.barge@gmail.com>
2024-12-16 13:30:21 -0500

    Removed luasnip because I'm not using it

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -133,21 +133,4 @@ return {
 	},
 	{ 'hrsh7th/cmp-nvim-lsp' },
 	{ 'saadparwaiz1/cmp_luasnip' },
-	{
-		'L3MON4D3/LuaSnip',
-		version = "v2.*",
-		build = "make install_jsregexp",
-		config = function()
-			local ls = require('luasnip')
-			vim.keymap.set({ "i" }, "<C-K>", function() ls.expand() end, { silent = true })
-			vim.keymap.set({ "i", "s" }, "<C-L>", function() ls.jump(1) end, { silent = true })
-			vim.keymap.set({ "i", "s" }, "<C-J>", function() ls.jump(-1) end, { silent = true })
-
-			vim.keymap.set({ "i", "s" }, "<C-E>", function()
-				if ls.choice_active() then
-					ls.change_choice(1)
-				end
-			end, { silent = true })
-		end
-	},
 }

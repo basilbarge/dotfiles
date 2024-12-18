9e06e8e01e28d698537ce9f1c15e7e8f48f7b485
basilbarge <zachb.barge@gmail.com>
2024-12-18 16:23:44 -0500

    Updated treesitter setup so I can install languages for highlighting

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,7 +1,17 @@
 return {
 	{
 		'nvim-treesitter/nvim-treesitter',
-		build = ":TSUpdate"
+		build = ":TSUpdate",
+		config = function ()
+			local configs = require("nvim-treesitter.configs")
+
+			configs.setup({
+				ensure_installed = { "go", "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end
 	},
 	'nvim-treesitter/nvim-treesitter-context',
 	'williamboman/mason-lspconfig.nvim',

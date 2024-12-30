07f925f23afa24174d5f8a3a1c16d9b5abdd7d43
basilbarge <zachb.barge@gmail.com>
2024-12-30 08:34:30 -0500

    Updated lualine config to get rid of separators. I think this looks nicer

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -5,9 +5,9 @@ return {
 		require('lualine').setup {
 			options = {
 				icons_enabled = true,
-				theme = 'catppuccin',
-				component_separators = { left = '', right = '' },
-				section_separators = { left = '', right = '' },
+				theme = 'auto',
+				component_separators = { left = '', right = '' },
+				section_separators = { left = '', right = '' },
 				disabled_filetypes = {
 					statusline = {},
 					winbar = {},
@@ -21,12 +21,31 @@ return {
 					winbar = 1000,
 				}
 			},
+
+			-- Available sections are
+			-- `branch` (git branch)
+			-- `buffers` (shows currently available buffers)
+			-- `diagnostics` (diagnostics count from your preferred source)
+			-- `diff` (git diff status)
+			-- `encoding` (file encoding)
+			-- `fileformat` (file format)
+			-- `filename`
+			-- `filesize`
+			-- `filetype`
+			-- `hostname`
+			-- `location` (location in file in line:column format)
+			-- `mode` (vim mode)
+			-- `progress` (%progress in file)
+			-- `searchcount` (number of search matches when hlsearch is active)
+			-- `selectioncount` (number of selected characters or lines)
+			-- `tabs` (shows currently available tabs)
+			-- `windows` (shows currently available windows)
 			sections = {
 				lualine_a = { 'mode' },
 				lualine_b = { 'branch', 'diff', 'diagnostics' },
 				lualine_c = { 'filename' },
 				lualine_x = { 'encoding', 'fileformat', 'filetype' },
-				lualine_y = { 'progress' },
+				lualine_y = { 'progress', 'selectioncount' },
 				lualine_z = { 'location' }
 			},
 			inactive_sections = {

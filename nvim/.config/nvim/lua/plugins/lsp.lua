26107fbea8073ff05d5ff5979a8a60743f18312a
basilbarge <zachb.barge@gmail.com>
2025-01-02 15:18:10 -0500

    Re-introduced mason and mason lsp config for easier default lsp setup

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,27 +1,31 @@
 return {
-	'neovim/nvim-lspconfig',
+	"neovim/nvim-lspconfig",
 	dependencies = {
-		{
-			"folke/lazydev.nvim",
-			ft = "lua", -- only load on lua files
-			opts = {
-				library = {
-					-- See the configuration section for more details
-					-- Load luvit types when the `vim.uv` word is found
-					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-				},
-			},
-		},
-		{ 'saghen/blink.cmp' },
+		"saghen/blink.cmp",
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
 	},
 	config = function()
-		local capabilities = require('blink.cmp').get_lsp_capabilities()
-		require 'lspconfig'.gopls.setup {
-			capabilities = capabilities,
-		}
+		require("mason").setup()
+		require("mason-lspconfig").setup()
 
-		require 'lspconfig'.lua_ls.setup {
-			capabilities = capabilities,
+		local capabilities = require('blink.cmp').get_lsp_capabilities()
+
+		require("mason-lspconfig").setup_handlers {
+			-- The first entry (without a key) will be the default handler
+			-- and will be called for each installed server that doesn't have
+			-- a dedicated handler.
+			function(server_name) -- default handler (optional)
+				require("lspconfig")[server_name].setup {
+					capabilities = capabilities
+				}
+			end,
+
+			-- Next, you can provide a dedicated handler for specific servers.
+			-- For example, a handler override for the `rust_analyzer`:
+			-- ["rust_analyzer"] = function()
+			-- 	require("rust-tools").setup {}
+			-- end
 		}
 
 		-- Use LspAttach autocommand to only map the following keys

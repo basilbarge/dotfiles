f8dc749d0247820237335e34e49b066d43aaefca
Zach Barge <zachb.barge@gmail.com>
2025-05-29 10:31:20 -0400

    fix(nvim-config): Removed mason and blink functions to fix errors for integrating with nvim-lsp with newest version of nvim

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -9,25 +9,6 @@ return {
 		require("mason").setup()
 		require("mason-lspconfig").setup()
 
-		local capabilities = require('blink.cmp').get_lsp_capabilities()
-
-		require("mason-lspconfig").setup_handlers {
-			-- The first entry (without a key) will be the default handler
-			-- and will be called for each installed server that doesn't have
-			-- a dedicated handler.
-			function(server_name) -- default handler (optional)
-				require("lspconfig")[server_name].setup {
-					capabilities = capabilities
-				}
-			end,
-
-			-- Next, you can provide a dedicated handler for specific servers.
-			-- For example, a handler override for the `rust_analyzer`:
-			-- ["rust_analyzer"] = function()
-			-- 	require("rust-tools").setup {}
-			-- end
-		}
-
 		-- Use LspAttach autocommand to only map the following keys
 		-- after the language server attaches to the current buffer
 		vim.api.nvim_create_autocmd('LspAttach', {

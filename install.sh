282cb849c37a26d18f2cbba11c5b05268531530b
Zach Barge <zachb.barge@gmail.com>
2025-10-31 13:57:11 -0400

    fix(install): Updated installation method for eza in install.sh

--- install.sh
+++ install.sh
@@ -22,6 +22,15 @@ if [[ $NAME = "Ubuntu" ]]; then
 
 	BAT_NAME="batcat"
 
+	# Install eza
+	yes | sudo apt install gpg
+	sudo mkdir -p /etc/apt/keyrings
+	wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+	echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+	sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+	sudo apt update
+	sudo apt install -y eza
+
 	# Update tldr doc repository
 	tldr -u
 

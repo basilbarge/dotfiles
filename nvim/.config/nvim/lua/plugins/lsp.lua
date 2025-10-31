ebc13e9f081e4f27dbbf291639db8b436a6fe792
Zach Barge <zachb.barge@gmail.com>
2025-10-31 10:28:40 -0400

    refactor(lsp): implemented mini lsp picker for viewing references

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -19,6 +19,8 @@ return {
 				-- Buffer local mappings.
 				-- See `:help vim.lsp.*` for documentation on any of the below functions
 				local opts = { buffer = ev.buf }
+				local mini = require("mini.extra")
+
 				vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
 				vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
 				vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
@@ -32,7 +34,9 @@ return {
 				vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
 				vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
 				vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-				vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+				vim.keymap.set('n', '<leader>gr', function()
+					mini.pickers.lsp({ scope = "references" }, nil)
+				 end , opts)
 				vim.keymap.set('n', '<space>f', function()
 					vim.lsp.buf.format { async = true }
 				end, opts)

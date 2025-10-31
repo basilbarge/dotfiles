20009b2055a10778d2a43eb290d6df8548587671
Zach Barge <zachb.barge@gmail.com>
2025-10-31 11:38:32 -0400

    fix(wezterm): removed wezterm from install and config

--- install.sh
+++ install.sh
@@ -25,12 +25,6 @@ if [[ $NAME = "Ubuntu" ]]; then
 	# Update tldr doc repository
 	tldr -u
 
-	# Install Wezterm
-	#$curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
-	#echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
-	#sudo apt update
-	#sudo apt install wezterm
-
 	# Install prequisites for neovim to build from source
 	yes | sudo apt-get install ninja-build gettext cmake unzip curl
 	yes | sudo apt-get install make cmake gettext
--- wezterm/.wezterm.lua
+++
@@ -1,9 +0,0 @@
-local wezterm = require 'wezterm'
-local config = wezterm.config_builder()
-
--- Configure fonts
-config.font = wezterm.font 'MonaspiceKr Nerd Font Mono'
-config.color_scheme = "Catppuccin Mocha"
-
-return config
-

0866344f273889bd6ff7e5103f8b59255365f41a
Zach Barge <zachb.barge@gmail.com>
2025-11-17 13:32:53 -0500

    feat(install): Added function that display installation section

--- installation/functions.sh
+++ installation/functions.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 function exists() {
 	return $(command -v $1 >/dev/null 2>&1)
 }
@@ -14,4 +15,53 @@ function gh_install() {
 		&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 		&& sudo apt update \
 		&& sudo apt install gh -y
+}
+
+function demarcate() {
+	shopt -s checkwinsize
+	(:)
+
+	local title=$1
+	local title_length=${#title}
+
+	local halfway=$((COLUMNS / 2))
+	local title_start=$((halfway - (($title_length / 2))))
+	local title_end=$((halfway + (($title_length / 2))))
+	local section_char='*'
+	local empty_char=' '
+
+	local padding=4
+	 local s=''
+
+	for ((i = 0; i < $((COLUMNS)); i++)); do
+		s+=$section_char
+	done
+
+	printf "%s" "$s"
+
+	s=''
+
+	for ((i = 0; i < $(($title_start - $padding)); i++)); do
+		s+=$section_char
+	done
+	for ((i = $(($title_start - $padding)); i < $title_start; i++)); do
+		s+=$empty_char
+	done
+	s+=$title
+	for ((i = $title_end; i < $(($title_end + $padding)); i++)); do
+		s+=$empty_char
+	done
+	for ((i = $(($title_end + $padding)); i < $((COLUMNS)); i++)); do
+		s+=$section_char
+	done
+	  
+	printf "%s" "$s"
+
+	s=''
+
+	for ((i = 0; i < $((COLUMNS)); i++)); do
+		s+=$section_char
+	done
+
+	printf "%s" "$s"
 }

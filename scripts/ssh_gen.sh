e873fbb236626240a18405db30d97bc0ba36256d
Zach Barge <zachb.barge@gmail.com>
2025-11-20 22:10:49 -0500

    feat(ssh_gen): Take email for ssh key as an arg with getopts

--- scripts/ssh_gen.sh
+++ scripts/ssh_gen.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
 
+while getopts 'e:' opt; do
+	case "${opt}" in
+		e) email=$OPTARG ;;
+		*) fatal 'bad option' ;;
+ esac
+done
+
+if [[ $OPTIND = 1 ]]; then
+	echo "No email given for ssh key"
+	exit 1
+fi
+
 echo -n "Enter filename for ssh key:" 
 
 read ssh_file
 
 # Generate ssh key using script parameter as email
-ssh-keygen -t ed25519 -C $1 -f "/home/basilbarge/.ssh/$ssh_file"
+ssh-keygen -t ed25519 -C $email -f "/home/basilbarge/.ssh/$ssh_file"
 
 eval "$(ssh-agent -s)"
+
 
 ssh-add "/home/basilbarge/.ssh/$ssh_file"

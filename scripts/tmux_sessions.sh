4b4c358940643f7f72f59dc5e4f6bae464b48ddc
Zach Barge <zachb.barge@gmail.com>
2025-11-26 13:09:48 -0500

    feat(scripts): Added tmux_sessions to use find and fzf to open tmux in a specific project directory

---
+++ scripts/tmux_sessions.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+dirs=("${SESSION_DIRS:-"/home/$USER"}" "/home/$USER")
+
+dir_list=$(echo "${dirs[@]}")
+
+chosen_dir=$(find $dir_list -mindepth 1 -maxdepth 1 -type d | fzf)
+
+if [[ ! -d $chosen_dir ]]; then
+	echo "Chosen directory doesn't exist"
+	exit 1
+fi
+
+tmux new-session -A -s $(basename "$chosen_dir") -c "$chosen_dir"

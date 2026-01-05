5ae6cc94f4135af8b39d167aba6e5a7a6e9b70ba
Zach Barge <zachb.barge@gmail.com>
2026-01-05 15:52:48 -0500

    feat(nvim): Added treesitter back into configuration

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,17 +1,15 @@
 return {
---	{
---		'nvim-treesitter/nvim-treesitter',
---		build = ":TSUpdate",
---		config = function()
---			local configs = require("nvim-treesitter.configs")
---
---			configs.setup {
---				ensure_installed = { "go", "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
---				sync_install = false,
---				highlight = { enable = true },
---				--indent = { enable = true },
---			}
---		end
---	},
---	'nvim-treesitter/nvim-treesitter-context',
+	{
+		'nvim-treesitter/nvim-treesitter',
+		build = ":TSUpdate",
+		config = function()
+			require'nvim-treesitter'.setup {
+				ensure_installed = { "go", "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+				sync_install = false,
+				highlight = { enable = true },
+				--indent = { enable = true },
+			}
+		end
+	},
+	'nvim-treesitter/nvim-treesitter-context',
 }

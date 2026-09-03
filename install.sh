7df8717be7052699bb1effc5517f68945149d187
Zach Barge <zachb.barge@gmail.com>
2026-09-02 22:53:34 -0400

    feat(scripts, install): Changed hardcoded user for $USER var

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-source "/home/basilbarge/dotfiles/installation/install_packages"
-source "/home/basilbarge/dotfiles/installation/env_config"
-source "/home/basilbarge/dotfiles/installation/nvim" -s
-source "/home/basilbarge/dotfiles/installation/git_config"
-source "/home/basilbarge/dotfiles/installation/node"
+source "/home/$USER/dotfiles/installation/install_packages"
+source "/home/$USER/dotfiles/installation/env_config"
+source "/home/$USER/dotfiles/installation/nvim" -s
+source "/home/$USER/dotfiles/installation/git_config"
+source "/home/$USER/dotfiles/installation/node"
